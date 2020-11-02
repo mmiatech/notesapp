e0b84c035df0363bdef2e9a7dd897a4e2573576c
mmiatech <mmiatech@madisoncollege.edu>
2020-11-02 13:17:22 -0600

    attempt github pages publish

---
+++ docs/precache-manifest.a4e2c2d3999dc47b8d7a892c9110b6e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d782254cd9427cacc2ec84d29fec48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23b9b10c18c74c04379f",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "8fad893c6ba78c2d848b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23b9b10c18c74c04379f",
+    "url": "/static/js/2.13bf3f25.chunk.js"
+  },
+  {
+    "revision": "7c68b657c7cc254f4ca9b56af0cb6fab",
+    "url": "/static/js/2.13bf3f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fad893c6ba78c2d848b",
+    "url": "/static/js/main.b6881df0.chunk.js"
+  },
+  {
+    "revision": "6ee5350053857df44f7f",
+    "url": "/static/js/runtime-main.dc9e925e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
